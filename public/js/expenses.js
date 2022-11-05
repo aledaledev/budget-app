2a275919ce420cbd686fb1126cf3942674cf740b
aledale <ale.salas271102@gmail.com>
2022-11-04 23:49:19 -0300

    adding services

---
+++ public/js/expenses.js
@@ -0,0 +1,70 @@
+"use strict";
+class ArrayList {
+    constructor() {
+        this.items = [];
+    }
+    //ts infiere que es un void
+    add(item) {
+        this.items.push(item);
+    }
+    get(index) {
+        const item = this.items.filter((x, y) => y === index);
+        if (item.length === 0)
+            return null;
+        return item[0];
+    }
+    createFrom(value) {
+        this.items = [...value];
+    }
+    getAll() {
+        return this.items;
+    }
+}
+class Expenses {
+    constructor(currency) {
+        this.id = "id" + Math.random().toString(16).slice(2);
+        this.finalCurrency = currency;
+        this.expenses = new ArrayList();
+    }
+    add(item) {
+        this.expenses.add(item);
+        return true;
+    }
+    getItems() {
+        return this.expenses.getAll();
+    }
+    get(index) {
+        return this.expenses.get(index);
+    }
+    getTotal() {
+        const total = this.expenses.getAll().reduce((acc, item) => (acc += this.convertCurrency(item, this.finalCurrency)), 0);
+        return `${this.finalCurrency} $${total.toFixed(2).toString}`;
+    }
+    convertCurrency(item, currency) {
+        switch (item.cost.currency) {
+            case "USD":
+                switch (currency) {
+                    case "ARS":
+                        return item.cost.number * 290;
+                        break;
+                    default:
+                        return item.cost.number;
+                }
+                break;
+            case 'ARS':
+                switch (currency) {
+                    case "USD":
+                        return item.cost.number / 290;
+                        break;
+                    default:
+                        return item.cost.number;
+                }
+                break;
+            default:
+                return 0;
+        }
+    }
+    remove(id) {
+        throw new Error("Method not implemented.");
+    }
+}
--- src/expenses.ts
+++ src/expenses.ts
@@ -1,16 +1,100 @@
-type Currency = 'USD'|'ARS'|'SOL' 
+type Currency = "USD" | "ARS";
 
-interface Price{
-    number:number,
-    currency:Currency
+interface Price {
+  number: number;
+  currency: Currency;
 }
 
-interface ExpenseItem{
-    id:number,
-    title:string,
-    cost:Price
+interface ExpenseItem {
+  id: number;
+  title: string;
+  cost: Price;
 }
 
-class Expenses{
+interface IExpenses {
+    //no se implementa convertCurrency por que este ultimo es mas especifico
+  expenses: ArrayList<ExpenseItem>;
+  finalCurrency: Currency;
+  add(item: ExpenseItem): boolean;
+  get(index: number): ExpenseItem | null;
+  getTotal(): string;
+  remove(id: number): boolean;
+}
 
+class ArrayList<T> {
+  //recibira elemento generico(se definira descpues)
+  private items: T[];
+  constructor() {
+    this.items = [];
+  }
+  //ts infiere que es un void
+  add(item: T) {
+    this.items.push(item);
+  }
+  get(index: number): T | null {
+    const item: T[] = this.items.filter((x, y) => y === index);
+    if (item.length === 0) return null;
+    return item[0];
+  }
+  createFrom(value: T[]) {
+    this.items = [...value];
+  }
+  getAll(): T[] {
+    return this.items;
+  }
+}
+
+class Expenses implements IExpenses {
+  expenses: ArrayList<ExpenseItem>;
+  finalCurrency: Currency;
+
+  private id: string = "id" + Math.random().toString(16).slice(2);
+
+  constructor(currency: Currency) {
+    this.finalCurrency = currency;
+    this.expenses = new ArrayList<ExpenseItem>();
+  }
+  add(item: ExpenseItem): boolean {
+    this.expenses.add(item);
+    return true;
+  }
+  getItems(): ExpenseItem[] {
+    return this.expenses.getAll();
+  }
+  get(index: number): ExpenseItem | null {
+    return this.expenses.get(index);
+  }
+  getTotal(): string {
+    const total = this.expenses.getAll().reduce(
+        (acc, item) => (acc += this.convertCurrency(item, this.finalCurrency))
+      ,0);
+    return `${this.finalCurrency} $${total.toFixed(2).toString}`;
+  }
+  private convertCurrency(item: ExpenseItem, currency: Currency) {
+    switch (item.cost.currency) {
+      case "USD":
+        switch (currency) {
+          case "ARS":
+            return item.cost.number * 290;
+            break;
+          default:
+            return item.cost.number;
+        }
+      break;
+      case 'ARS':
+        switch (currency) {
+            case "USD":
+              return item.cost.number / 290;
+              break;
+            default:
+              return item.cost.number;
+          }
+      break;
+      default:
+        return 0
+    }
+  }
+  remove(id: number): boolean {
+    throw new Error("Method not implemented.");
+  }
 }
